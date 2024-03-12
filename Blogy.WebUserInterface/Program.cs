49c12e598e5ee59aa6fe4d94e61dd8b351b1dad3
Yahya Erdogan <yahyacanerdogan@outlook.com>
2024-03-12 07:26:37 -0500

    Dependency enjection created.

--- Blogy.WebUserInterface/Program.cs
+++ Blogy.WebUserInterface/Program.cs
@@ -1,6 +1,17 @@
+using Blogy.BussinessLayer.Abstract;
+using Blogy.BussinessLayer.Concrete;
+using Blogy.DataAccessLayer.Abstract;
+using Blogy.DataAccessLayer.Context;
+using Blogy.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+#region Dependency enjection
+builder.Services.AddDbContext<BlogyContext>();
+builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+#endregion
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
