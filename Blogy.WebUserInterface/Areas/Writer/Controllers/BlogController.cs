1b21cf049f7e2023b203fcc48a9e64a1888fd380
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-18 07:02:10 -0500

    Spaces deleted.

--- Blogy.WebUserInterface/Areas/Writer/Controllers/BlogController.cs
+++ Blogy.WebUserInterface/Areas/Writer/Controllers/BlogController.cs
@@ -21,7 +21,7 @@ namespace Blogy.WebUserInterface.Areas.Blogy.WriterArea.Controllers
         public async Task<IActionResult> MyBlogList()
         {
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
-            ViewBag.UserInfo = user.FirstName + " " + user.LastName;
+            //ViewBag.UserInfo = user.FirstName + " " + user.LastName;
             var values = _articleService.TGetArticlesByWriterId(user.Id);
             return View(values);
         }
