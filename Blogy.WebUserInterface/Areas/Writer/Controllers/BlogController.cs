e470c2c2e7f74e2d27cce5020cd4d73df1ab6a25
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-19 01:29:58 -0500

    Category listed, new blog created.

--- Blogy.WebUserInterface/Areas/Writer/Controllers/BlogController.cs
+++ Blogy.WebUserInterface/Areas/Writer/Controllers/BlogController.cs
@@ -2,6 +2,7 @@
 using Blogy.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Blogy.WebUserInterface.Areas.Blogy.WriterArea.Controllers
 {
@@ -9,13 +10,15 @@ namespace Blogy.WebUserInterface.Areas.Blogy.WriterArea.Controllers
     public class BlogController : Controller
     {
         private readonly IArticleService _articleService;
+        private readonly ICategoryService _categoryService;
         private readonly UserManager<AppUser> _userManager;
 
 
-        public BlogController(IArticleService articleService, UserManager<AppUser> userManager)
+        public BlogController(IArticleService articleService, UserManager<AppUser> userManager, ICategoryService categoryService)
         {
             _articleService = articleService;
             _userManager = userManager;
+            _categoryService = categoryService;
         }
 
         public async Task<IActionResult> MyBlogList()
@@ -24,6 +27,42 @@ namespace Blogy.WebUserInterface.Areas.Blogy.WriterArea.Controllers
             //ViewBag.UserInfo = user.FirstName + " " + user.LastName;
             var values = _articleService.TGetArticlesByWriterId(user.Id);
             return View(values);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateArticle()
+        {
+            GetCategoryName();
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateArticle(Article article)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            article.AppUserId = values.Id;
+            article.WriterId = 1;
+            article.CreatedDate = DateTime.Now;
+            _articleService.TAdd(article);
+                    
+            return RedirectToAction("MyBlogList", "Blog");
+        }
+
+        public ActionResult DeleteArticle(int id)
+        {
+             _articleService.TDelete(id);
+            return RedirectToAction("MyBlogList", "Blog");
+        }
+        public void GetCategoryName()
+        {
+            ViewBag.CategoryName = new SelectList(_categoryService.TGetListAll(), "CategoryId", "Name");
+
+            List<SelectListItem> selectListItems = (from i in _categoryService.TGetListAll()
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = i.Name,
+                                                        Value = i.CategoryId.ToString()
+                                                    }).ToList();
+            ViewBag.CategoryName = selectListItems;
         }
     }
 }
